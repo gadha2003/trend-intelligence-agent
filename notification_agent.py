9edebbc69bdc09419bc1f88a55c079ee81a94146
Gadha Suresh <142001881+gadha2003@users.noreply.github.com>
2026-02-27 00:04:13 +0530

    Update notification_agent.py

--- notification_agent.py
+++ notification_agent.py
@@ -1,14 +1,13 @@
+import os
 import smtplib
 import sqlite3
 from email.mime.text import MIMEText
 
-EMAIL = "gadhasuresh2003@gmail.com"
-APP_PASSWORD = "quks iruj cazy zugs"
+EMAIL = os.environ.get("EMAIL")
+APP_PASSWORD = os.environ.get("APP_PASSWORD")
 
 def send_email():
-
     conn = sqlite3.connect("trends.db")
-
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -25,19 +24,12 @@ def send_email():
         message += f"- {trend[0]}\n"
 
     msg = MIMEText(message)
-
     msg["Subject"] = "AI Trend Agent Alert"
     msg["From"] = EMAIL
     msg["To"] = EMAIL
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
-
     server.starttls()
-
     server.login(EMAIL, APP_PASSWORD)
-
     server.send_message(msg)
-
     server.quit()
-
-    print("Email notification sent")

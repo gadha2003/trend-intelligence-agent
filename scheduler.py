c1ff5a7121e4e6eda8b7c2b82d3a1d3bf250d5e1
Gadha Suresh <142001881+gadha2003@users.noreply.github.com>
2026-03-03 13:49:34 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,11 @@
 from agents import google_trends_agent
 from notification_agent import send_email
 
+
 def run_agents():
-    print("Running Trend Intelligence Agent...")
     google_trends_agent()
     send_email()
-    print("Run complete.")
+
 
 if __name__ == "__main__":
     run_agents()
